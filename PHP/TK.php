29ee5fbe59d6da11d1293f205cf9a58577033f15
Jeremy Breen <jbree857@mail.endicott.edu>
2016-02-24 19:58:08 +0000

    Made changes to PHP file and HTML file that allows data to transfer from server side code to front end

--- PHP/TK.php
+++ PHP/TK.php
@@ -2,29 +2,30 @@
 
 $searchResult = 'null';
 
-if ($_SERVER["REQUEST_METHOD"] == "GET")
-{
-	$searchText = $_GET["searchText"];
+//if ($_SERVER["REQUEST_METHOD"] == "GET")
+//$searchText = $_GET["searchText"];
 
-	if (empty($searchText))
-	{
-		$searchResult = 'No search text submitted';
-	}
-	else
-	{
-		$searchResult = 'Search text submitted';
+//	if (empty($searchText))
+//	{
+//		$searchResult = 'No search text submitted';
+//	}
+//	else
+//	{
+//		$searchResult = 'Search text submitted';
 		//$searchResult = getResults($searchText);
-	}
-}
-else
-{
-	$searchResult = 'request was not GET';
-}
+//	}
+//}
+//else
+//{
+//	$searchResult = 'request was not GET';
+//}
+$result =  array(
+	//"searchResult" => $searchResult,
+	"searchResult" => "stuff",
+	"search" => $searchResult
 
-return array(
+);
 
-	"searchResult" => $searchResult,
-
-	);
+echo json_encode($result);
 
 ?>
